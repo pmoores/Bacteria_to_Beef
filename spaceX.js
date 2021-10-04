4687cbf521f1c2e3c827bb44b0fe3885f1edaeff
Patrick Moores <patrickmoores@Patricks-MacBook-Air.local>
2021-10-04 14:50:56 -0400

    Update after 12.4.1

--- spaceX.js
+++ spaceX.js
@@ -1,3 +1,11 @@
-const url = "https://api.spacexdata.com/v2/launchpads";
+// 12.3.1 Inspect an API call with D3.json()
+// const url = "https://api.spacexdata.com/v2/launchpads";
+// d3.json(url).then(receivedData => console.log(receivedData));
 
-d3.json(url).then(receivedData => console.log(receivedData));
+
+// 12.3.2 Load a JSON file with D3.json()
+// Must run 'python -m http.server' in Terminal when loading an external file into a Javascript file
+d3.json("samples.json").then(function(data){
+    console.log(data);
+});
+
